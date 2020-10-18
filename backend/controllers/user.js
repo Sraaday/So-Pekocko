d3667f94b7d86f2a63d7531ffb9e26b9cee29efa
Sraaday <60046022+Sraaday@users.noreply.github.com>
2020-10-18 17:24:56 +0200

    +login +auth Json Web Token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -23,7 +23,29 @@ exports.signup = (req, res) => {
 
 };
 
-exports.login = (req, res) => {
-    console.log("[controllers user.js] login")
-    return res.status(200).json({texte : "ça marche "}).end();
-}
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if (!user){
+            return res.status(401).json({ error: 'Utilisateur introuvable !'});
+        }
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            if (!valid) {
+                return res.status(401).json({ error: 'Mot de passe incorrect !'});
+            }
+            res.status(200).json({
+                userId: user._id,
+                token: jwt.sign(
+                    { userId: user._id },
+                    'RANDOM_TOKEN_SECRET',
+                    { expiresIn: '24h' }
+                  )
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+
+
+};
