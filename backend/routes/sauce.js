14f7576096a0a278c45f1c8179609e7f6af6ac55
Sraaday <60046022+Sraaday@users.noreply.github.com>
2020-10-22 17:52:06 +0200

    +UpdateSauce +Delete Sauce

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -8,8 +8,11 @@ const sauceCtrl = require('../controllers/sauce');
 
 
 
-router.post('/', auth, multer, sauceCtrl.createSauce );
-router.get('/',auth, sauceCtrl.getAllSauce);  
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.get('/',auth, sauceCtrl.getAllSauce);
+router.get('/:id',auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id',auth, sauceCtrl.deleteSauce);  
 
 
 module.exports = router;
